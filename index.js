150002ab3af18bd9501564d9e681868d718d43ee
abdelrahman ibrahem <abdo.ibrahem1122@gmail.com>
2022-02-23 20:48:10 +0200

    upload project files

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.use((req , res , next)=>{
 });
 
 // run Application
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port , ()=>{
     if (process.env.NODE_ENV === 'development')
         console.log('App is Running');
